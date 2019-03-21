f91ef11167e5a9f36417fcfa2657e925ee5faacd
Madison Hanberry <mhanberry@protonmail.com>
2019-03-21 18:15:09 -0400

    added scrolling to the menu

--- js/generateMenu.js
+++ js/generateMenu.js
@@ -6,8 +6,20 @@ modularjs.doOnceLoaded.push(
 
 		// Create a menu entry for each section
 		for(var i = 0; i < sections.length; i++){
+			var offsetTop = sections[i].offsetTop - 40;
+			var options = "{" +
+				"'top':" + offsetTop + "," +
+				"'behavior':'smooth'" +
+			"}"; 
+			var onclick = '"window.scroll(' + options + ')"';
 			var header = sections[i].firstElementChild.firstElementChild.textContent;
-			menu.getElementsByTagName("ul")[0].innerHTML += "<li><span>▸</span>" + header + "</li>";
+			menu.getElementsByTagName("ul")[0].innerHTML += "<li onclick=" + onclick + ">" +
+				"<span>▸</span>" + header +
+			"</li>";
 		}
 	}
 );
+
+function scrollTo(id){
+	
+}
